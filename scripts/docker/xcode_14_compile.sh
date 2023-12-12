fb43e5b773187dd83849d5113313bf887fa1cbe8
Sergi Sevil <sergi.sevil@build38.com>
2023-12-12 11:51:18 +0100

    Add file permissions

--- scripts/docker/xcode_14_compile.sh
+++ scripts/docker/xcode_14_compile.sh
@@ -70,4 +70,5 @@ cd ..
 
 lipo -create -output omvll.dylib ./arm64/libOMVLL.dylib ./x86_64/libOMVLL.dylib
 mv /o-mvll/src/build_xcode/omvll.dylib /o-mvll/src/build_xcode/omvll_unsigned.dylib
-#chown -R 1000:1000 /o-mvll/src/build_xcode
+chown -R 1000:1000 /o-mvll/src/build_xcode
+chmod -R 755 /o-mvll/src/build_xcode
